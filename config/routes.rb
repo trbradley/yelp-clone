2e86c5222c0c0f4bfee7a470044764c0cf9eaef0
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-21 16:16:18 +0000

    Fixes routes error

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
-  root to: 'restaurants#index'
   end
+
+  root to: 'restaurants#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
