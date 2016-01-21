e6a6b55675be5225030b4d1f666a986bbaad5581
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-21 14:03:49 +0000

    Adds user model and feature tests for sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :restaurants do
     resources :reviews
+  root to: 'restaurants#index'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
