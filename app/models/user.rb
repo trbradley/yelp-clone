790faecd47ddcad49ad435186d93795b6954b31a
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-22 11:09:19 +0000

    Adds endorsement test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,10 @@ class User < ActiveRecord::Base
   end
 
   def self.new_with_session(params, session)
-  super.tap do |user|
-    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
-end
 end
