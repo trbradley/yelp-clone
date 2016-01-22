ed064506043212dd87935116e28ea8324f21e064
michaellennox <michaelctlennox@gmail.com>
2016-01-22 15:30:40 +0000

    Adds database cleaner setup

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -6,9 +6,11 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  scenario 'a user can endorse a review, which increments the endorsement count', js: :true do
     visit '/restaurants'
+    expect(page).to have_content('0 endorsements')
     click_link 'Endorse Review'
     expect(page).to have_content('1 endorsement')
   end
+
 end
